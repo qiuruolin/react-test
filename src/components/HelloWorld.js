7514b696742e7091a4c85f06050651eeee1f006e
qiuruolin <csu_qiuruolin@163.com>
2018-04-07 14:47:50 +0800

    test

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -3,7 +3,7 @@ import React from 'react';
 class HelloWorld extends React.Component{
     render(){
         return(
-            <div>Hello World</div>
+            <div>Hello sss </div>
         );
     }
 }
