c78dbed25425f02640b910a8c2ee8d874f4050b9
qiuruolin <csu_qiuruolin@163.com>
2018-04-17 23:35:18 +0800

    删除多余代码

--- src/action/action.js
+++ src/action/action.js
@@ -1,10 +1,5 @@
 import {SET_TOKEN} from './ActionType';
 
-// export const setToken = (user) => ({
-//     type: SET_TOKEN,
-//     ...user
-// })
-
 export const setToken = (user) => ({
         type: SET_TOKEN,
         user: user
